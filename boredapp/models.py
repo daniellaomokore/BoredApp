ba7309269db7a44fe992945a2a1f3a3fb099ea27
Daniella <79287671+daniellaomokore@users.noreply.github.com>
2023-05-24 08:26:49 +0100

    Update models.py

--- boredapp/models.py
+++ boredapp/models.py
@@ -1,25 +1,27 @@
-import sqlalchemy
 import psycopg2
-from boredapp import database
+from sqlalchemy import Column, Integer, String, ForeignKey, Index
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
 if __name__ == '__main__':
-    from config import DATABASEPASSWORD, DATABASENAME, HOST, USER
+    from config import DATABASEPASSWORD, DATABASENAME, HOST, USER, PORT
 else:
     from boredapp.config import DATABASEPASSWORD, DATABASENAME, USER, HOST, PORT
-from sqlalchemy import create_engine, orm, Index
 
-Base = sqlalchemy.orm.declarative_base()
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
 
 # TO CREATE THE DATABASE
-# Note: I put this in a function to prevent cnx from running when i run the app even when i haven't ran the models file.
+# Note: I put this in a function to prevent cnx from running when I run the app even when I haven't run the models file.
 def create_database():
     """
-    This function creates a new MYSQL database.
+    This function creates a new PostgreSQL database.
     """
 
     # Establish a connection to the PostgreSQL server
     cnx = psycopg2.connect(user=USER, password=DATABASEPASSWORD, host=HOST, port=PORT)
-
 
     # Create a cursor object to execute SQL commands
     cursor = cnx.cursor()
@@ -35,44 +37,41 @@ def create_database():
     cnx.close()
 
 
-# DATABASE TABLE SCHEMA
-
-Base = sqlalchemy.orm.declarative_base()
-
-
 class TheUsers(Base):
-    """
-        This function creates a new MYSQL database table.
-    """
     __tablename__ = 'the_users'
 
-    UserID = database.Column(database.Integer, primary_key=True, autoincrement=True, nullable=False)
-    FirstName = database.Column(database.String(65), nullable=False)
-    LastName = database.Column(database.String(65), nullable=False)
-    Email = database.Column(database.String(65), nullable=False, unique=True, index=True)  # create index for Email column
-    Username = database.Column(database.String(200), index=True)  # create index for Username column
-    Password = database.Column(database.String(200))
+    UserID = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    FirstName = Column(String(65), nullable=False)
+    LastName = Column(String(65), nullable=False)
+    Email = Column(String(65), nullable=False, unique=True, index=True)
+    Username = Column(String(200), index=True)
+    Password = Column(String(200))
 
-    def __init__(self, FirstName, LastName, Email, Username=None, Password=None): # Set Username & Password to Default None becuase if a user signs up with google account then those arguments aren't needed
+
+    def __init__(self, FirstName, LastName, Email, Username=None, Password=None):
         self.FirstName = FirstName
         self.LastName = LastName
         self.Email = Email
         self.Username = Username
         self.Password = Password
+        
+    # Add the relationship attributes
+    favourites = relationship('Favourites')
 
 
 class Favourites(Base):
-    """
-        This function creates a new MYSQL database table.
-    """
     __tablename__ = 'favourites'
 
-    activityID = database.Column(database.Integer, primary_key=True, nullable=False)
-    UserID = database.Column(database.Integer, database.ForeignKey('the_users.UserID'), nullable=False)
-    activity = database.Column(database.String(200), nullable=False)
-    participants = database.Column(database.Integer, nullable=False)
-    type = database.Column(database.String(200), nullable=False)
-    link = database.Column(database.String(200), nullable=False)
+    activityID = Column(Integer, primary_key=True, nullable=False)
+    UserID = Column(Integer, ForeignKey('the_users.UserID'), nullable=False)
+    activity = Column(String(200), nullable=False)
+    participants = Column(Integer, nullable=False)
+    type = Column(String(200), nullable=False)
+    link = Column(String(200), nullable=False)
+
+    __table_args__ = (
+        Index('ix_favourites_activityID_UserID', 'activityID', 'UserID'),
+    )
 
     def __init__(self, activityID, UserID, activity, participants, type, link):
         self.activityID = activityID
@@ -82,17 +81,16 @@ class Favourites(Base):
         self.type = type
         self.link = link
 
-        # create 2 column index for activity id and user id.
-        __table_args__ = (
-            Index('ix_favourites_activityID_UserID', 'activityID', 'UserID'),
-        )
+    # Add the relationship attributes
+    user = relationship('TheUsers')
 
 
-engine = create_engine("mysql+mysqlconnector://{user}:{password}@{host}/{DatabaseName}".format(
+engine = create_engine("postgresql+psycopg2://{user}:{password}@{host}:{port}/{databasename}".format(
     user=USER,
     password=DATABASEPASSWORD,
     host=HOST,
-    DatabaseName=DATABASENAME
+    port=PORT,
+    databasename=DATABASENAME
 ))
 
 
@@ -101,5 +99,5 @@ def create_database_table():
 
 
 if __name__ == "__main__":
-    create_database()l
+    create_database()
     create_database_table()
